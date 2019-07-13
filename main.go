8a875af7ba9c911dfbfc8300799717898057dce4
Mikita Kryvitski <solairerove@gmail.com>
2019-07-13 14:55:39 +0300

    feature: just init new jedi repo

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	ex1 "github.com/solairerove/go-ninja-lvl-7-playground/jedi1"
+	ex2 "github.com/solairerove/go-ninja-lvl-7-playground/jedi2"
+	ex3 "github.com/solairerove/go-ninja-lvl-7-playground/jedi3"
+)
+
+func main() {
+	fmt.Println("Jedi lvl 7")
+
+	ex1.Ex1()
+	ex2.Ex2()
+	ex3.Ex3()
+}
