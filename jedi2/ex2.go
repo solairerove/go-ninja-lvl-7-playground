4216316f68f6ba1bb95213635a140a97d64ad45b
Mikita Kryvitski <solairerove@gmail.com>
2019-07-13 15:58:42 +0300

    feature: pass argument by ref

--- jedi2/ex2.go
+++ jedi2/ex2.go
@@ -4,7 +4,29 @@ import (
 	"fmt"
 )
 
+type person struct {
+	first string
+	last  string
+	age   int
+}
+
 // Ex2 tbd
 func Ex2() {
 	fmt.Println("\nex2")
+
+	p := person{
+		first: "Jack",
+		last:  "Mental Illness",
+		age:   32,
+	}
+
+	fmt.Println(p)
+
+	changeMe(&p)
+
+	fmt.Println(p)
+}
+
+func changeMe(p *person) {
+	(*p).first = "Tyler"
 }
